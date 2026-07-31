a6a4620d697d73c2f40a014e88356eae377f60b4
gunjan <dhiman.gunjan07@gmail.com>
2026-07-31 21:43:00 +0530

    basic input output program

---
+++ basic/number.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cout<<"enter any number";
+    cin>>x;
+    cout <<"the number you entered is:"<<endl<<x;
+    return 0;
+}
